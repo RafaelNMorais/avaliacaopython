c6022cc6639663f29fc304fc5f7b465c96b6ac7a
Rafael Nunes Morais <rafael.morais_BOLS@pampa.compasso>
2019-06-26 15:52:33 -0300

    Atividade 02: start - Iniciando o jogo

---
+++ menu.py
@@ -0,0 +1,31 @@
+def iniciar_jogo():
+
+    inicio = input("1 - Iniciar Jogo, 2 - Sair")
+    if(inicio == "1"):
+        print("Start")
+        nome_jogador1 = input("Insira o nome do jogador 1")
+
+        personagens = inicia_personagem()
+
+    elif(inicio == "2"):
+        print("Encerrando")
+
+def inicia_personagem():
+    arquivo = open("personagens.txt", "r")
+    personagens = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        personagens.append(linha)
+
+    arquivo.close
+
+    personagem1 = personagens[0]
+    personagem2 = personagens[1]
+    personagem3 = personagens[2]
+    personagem4 = personagens[3]
+
+    return personagem1, personagem2, personagem3, personagem4
+
+if(__name__ == "__main__"):
+    iniciar_jogo()
