cff82dcfd9c7d8e56d8b90949557b3c34c79ad6b
Felipe Christiano Moeller Oliveira <felipe.oliveira_BOLS@pampa.compasso>
2019-06-27 16:53:27 -0300

    Atividade 3: welcome - Continuação do desenvolvimento das decisões

--- introducao.py
+++ introducao.py
@@ -1,16 +1,21 @@
 def iniciar_introducao():
-    print("O mundo foi devastado e agora é governardo pelo Rei Malvado. Resta a você salvar o reino... Na sua jornada para a batalha, há um caminho pela floresta ou pela montanha. Qual a sua escolha?")
-    print("Indo pela floresta, as árvores fazem barulho durante todo o percurso e, no fim do caminho, alguém surge por trás delas oferecendo alimento. ")
-    print("Indo pelas montanhas, que são frias, cheias de gelo, alguém aparece com um casaco no topo, oferecendo vestimenta.")
+    print("O mundo foi devastado e agora é governardo pelo Rei Malvado. Resta a você salvar o reino... Na sua jornada "
+          "para a batalha, há um caminho pela floresta ou pela montanha. Qual a sua escolha?")
+    print("Indo pela floresta, as árvores fazem barulho durante todo o percurso e, no fim do caminho, alguém surge por"
+          " trás delas oferecendo alimento. ")
+    print("Indo pelas montanhas, que são frias, cheias de gelo, alguém aparece com um casaco no topo,"
+          " oferecendo vestimenta.")
     print()
     print("Ao chegar no destino (a zona de guerra), o jogador recebe uma carta misteriosa e decide ler ela ou não.")
     print()
     print("Na zona de guerra, diversas batalhas contra os inimigos acontecem.")
     print()
-    print("Após as batalhas, os sobreviventes enfrentam conflitos internos como sequelas da guerra e lutam uma batalha final entre si.")
+    print("Após as batalhas, os sobreviventes enfrentam conflitos internos como sequelas da guerra e lutam uma batalha"
+          " final entre si.")
     print()
     print("O jogador vence se chega bem até essa parte.")
     print("Ou perde, se tiver morrido, se for atacado ou se estiver sozinho.")
+
 
 if(__name__ == "__main__"):
     iniciar_introducao()
--- jogo.py
+++ jogo.py
@@ -2,20 +2,19 @@ import introducao
 
 
 def iniciar_jogo():
-
     inicio = input("[1] - Iniciar Jogo\n"
                    "[2] - Sair\n"
                    "Escolha: ")
-    if(inicio == "1"):
-        print("Start")
+    if (inicio == "1"):
+        introducao.iniciar_introducao()
+        print()
         nome_jogador = input("Insira o nome do jogador: ")
         print()
 
         personagens = inicia_personagem()
-        introducao.iniciar_introducao()
         decisoes1()
 
-    elif(inicio == "2"):
+    elif (inicio == "2"):
         print("Encerrando")
 
 
@@ -34,7 +33,7 @@ def inicia_personagem():
     personagem3 = personagens[2]
     personagem4 = personagens[3]
     personagem5 = personagens[4]
-    personagem6 = personagens[6]
+    personagem6 = personagens[5]
 
     return personagem1, personagem2, personagem3, personagem4, personagem5, personagem6
 
@@ -53,18 +52,18 @@ def decisoes1():
                     "[2] - Atacar a Princesa Andromeda\n"
                     "Escolha: ")
 
-    if(escolha == "1"):
+    if (escolha == "1"):
         print()
         print("Você se levanta com um ar de glória e caminha em direção a Princesa.")
         print("A Princesa percebe você se aproximando e fala:")
         print("- Oh bravo aventureiro, está será um missão difícil.")
-        print("- Você deverá ir até o Obelisco do Espaço resgatar o Mestre dos Magos.")
+        print("- Você deverá ir até o Obelisco do Espaço resgatar Ilenor o Mago da corte.")
         print("- Ele foi raptado por um Ninja poderoso.")
         print("- Você deve seguir ao Norte até encontrar a Gruta de Drayhill.")
         print("- Ela é protegida pelo temido Marco Nove Vidas.")
         print("- Tenha cuidado guerreiro, que seu caminho seja ilumidado.")
         decisoes2()
-    elif(escolha == "2"):
+    elif (escolha == "2"):
         print()
         print("Vecê se levanta com raiva, saca sua espada, e corre em direção a princesa.")
         print("Os guardas percebem sua ação e puxam a princesa para trás deles e formam um barreira de escudos.")
@@ -74,7 +73,8 @@ def decisoes1():
 
 def decisoes2():
     print()
-    print("Você sai pela porta da taverna, se depara com a praça da cidade, ela está lotada de moradores e comerciantes")
+    print(
+        "Você sai pela porta da taverna, se depara com a praça da cidade, ela está lotada de moradores e comerciantes")
     print("Segue em frente pela portão principal, quando um guarda te para e fala:")
     print("- Ei você ai, você é o maluco que aceitou a missão da Princesa?")
     print("- Ah você tem culhões amigo...")
@@ -82,12 +82,12 @@ def decisoes2():
     escolha1 = input("[1] - Continuar andando e ignorar o guarda\n"
                      "[2] - Parar e ouvir o conselho do guarda\n"
                      "Escolha: ")
-    if(escolha1 == "1"):
+    if (escolha1 == "1"):
         print()
         print("Você segue andando pela estrada sem se importar com o guarda.")
         print("Enquanto isso o guarda te olha de longe com um rosto de preocupação.")
         print()
-    elif(escolha1 == "2"):
+    elif (escolha1 == "2"):
         print()
         print("Você para ao lado do guarda e ele te fala:")
         print("- Seguinte amigo, para chegar até a Gruta de Drayhill você tem dois caminhos.")
@@ -102,9 +102,13 @@ def decisoes2():
 
 def decisoes3():
     print()
+    print("Como você morou na cidade a vida toda, acaba tendo o conhecimento que, para chegar a Gruta de Drayhill"
+          " há dois caminhos a seguir: ")
+    print()
     escolha3 = input("[1] - Seguir o caminho pela estrada\n"
-                     "[2] - Seguir o caminho pela floresta")
-    if(escolha3 == "1"):
+                     "[2] - Seguir o caminho pela floresta\n"
+                     "Escolha: ")
+    if (escolha3 == "1"):
         print()
         print("Você caminha pela estrada, ela é bem populada, passa por alguns comerciantes e fazendeiros.")
         print("Já estava na caida da noite, a estrada começa a ficar vazia, tudo está escuro ao seu redor.")
@@ -116,14 +120,83 @@ def decisoes3():
         escolha4 = input("[1] - Investigar o barulho no mato\n"
                          "[2] - Ignorar o barulho e seguir em frente\n"
                          "Escolha: ")
-        if(escolha4 == "1"):
+        if (escolha4 == "1"):
             print()
-
-        elif(escolha4 == "2"):
+            print("Você se aproxima com cuidado do canto da estrada, vai afastando a grama alta com a mãos...")
+            print("De repente um bandido pula em sua direção... e fala:")
+            print("- Olha só oq eu encontrei por aqui, um engomadinho bisbilhoteiro...")
+            print("Ele te ameaça com uma espada curta empunhada.")
+            print("- É melhor ir passando suas coisas se não quiser problemas...")
+            # combate ou fugir
             print()
-    elif(escolha3 == "2"):
+        elif (escolha4 == "2"):
+            print()
+            print("Você acaba ficando assustado e acelera o passo, mas isso não foi o suficiente...")
+            print("O barulho começa a te seguir e ficar cada vez mais alto...")
+            print("Um bandido sai correndo do mato e para na sua frente...")
+            print("Com um olher ameaçador ele fala:")
+            print("- Ora ora oque temos aqui, um covarde querendo fugir da encrenca...")
+            print("- Já que o amigo aqui está com pressa, passe as suas coisas pra cá, se não quiser problemas...")
+            # combate ou fugir
+            print()
+    elif (escolha3 == "2"):
         print()
+        # caminho floresta
+
+        # combate ou fuga
+
+    print()
+    # chegada na gruta
+    decisoes4()
 
 
-if(__name__ == "__main__"):
+def decisoes4():
+    # chegada na gruta
+    escolha = input("[1] - Entrar na gruta sem auxilio de luz\n"
+                    "[2] - Acender uma tocha e entrar na gruta\n"
+                    "Escolha: ")
+    if (escolha == "1"):
+        print()
+        # sem tocha
+    elif (escolha == "2"):
+        print()
+    # com tocha
+
+    # combate com Marco Nove vidas
+    decisoes5()
+
+
+def decisoes5():
+    # chegada ao Obelisco do Espaço
+
+    escolha = input("[1] - Subir as escadas da torre com cuidado\n"
+                    "[2] - Subir as escadas da torre correndo\n"
+                    "Escolha: ")
+
+    if (escolha == "1"):
+        print()
+        # subir cuidado
+    elif (escolha == "2"):
+        print()
+        # subir correndo
+
+    # combate com Valazar
+
+
+def decisoes6():
+    print()
+    # encontro com o Ilenor o Mago
+
+    escolha = input("[1] - Voltar andando até a cidade\n"
+                    "[2] - Usar o portal que o Ilenor abriu")
+
+    if(escolha == "1"):
+        print()
+        # voltar andando
+    elif(escolha == "2"):
+        print()
+        # usar o portal
+        
+
+if (__name__ == "__main__"):
     iniciar_jogo()
