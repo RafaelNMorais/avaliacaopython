e8f8dd75d211012ab0846bd7e067072e95e026b2
Rafael Nunes Morais <rafael.morais_BOLS@pampa.compasso>
2019-06-28 16:38:18 -0300

    Atividade 04: Fight for life - Iniciando as batalhas

---
+++ batalhas.py
@@ -0,0 +1,72 @@
+import random
+
+def batalha_bandido():
+    hp_jogador = 100
+    hp_inimigo = 100
+    ataque_jogador = random.randrange(1,21)
+    ataque_inimigo = random.randrange(1,16)
+    hp_jogador = hp_jogador - ataque_inimigo
+    hp_inimigo = hp_inimigo - ataque_jogador
+    if (hp_inimigo < 0):
+        hp_inimigo = 0
+    elif (hp_jogador < 0):
+        hp_jogador = 0
+    print("Hp jogador = {}".format(hp_jogador))
+    print(ataque_jogador)
+    print("Hp inimigo = {}".format(hp_inimigo))
+    print(ataque_inimigo)
+    continua = input("1- Continua")
+
+def batalha_artorias():
+    hp_jogador = 100
+    hp_inimigo = 150
+    ataque_jogador = random.randrange(21)
+    ataque_inimigo = random.randrange(31)
+    while(hp_jogador > 0 and hp_inimigo > 0):
+        hp_jogador = hp_jogador - ataque_inimigo
+        hp_inimigo = hp_inimigo - ataque_jogador
+        if (hp_inimigo < 0):
+            hp_inimigo = 0
+        elif (hp_jogador < 0):
+            hp_jogador = 0
+        print("Hp jogador = {}".format(hp_jogador))
+        print(ataque_jogador)
+        print("Hp inimigo = {}".format(hp_inimigo))
+        print(ataque_inimigo)
+        continua = input("1- Continua")
+
+def batalha_marco():
+    hp_jogador = 100
+    hp_inimigo = 100
+    ataque_jogador = random.randrange(1,21)
+    ataque_inimigo = random.randrange(1,21)
+    while(hp_jogador or hp_inimigo > 0):
+        hp_jogador = hp_jogador - ataque_inimigo
+        hp_inimigo = hp_inimigo - ataque_jogador
+        if (hp_inimigo < 0):
+            hp_inimigo = 0
+        elif (hp_jogador < 0):
+            hp_jogador = 0
+        print("Hp jogador = {}".format(hp_jogador))
+        print(ataque_jogador)
+        print("Hp inimigo = {}".format(hp_inimigo))
+        print(ataque_inimigo)
+        continua = input("1- Continua")
+
+def batalha_velazar():
+    hp_jogador = 100
+    hp_inimigo = 150
+    ataque_jogador = random.randrange(1,21)
+    ataque_inimigo = random.randrange(1,31)
+    while(hp_jogador or hp_inimigo > 0):
+        hp_jogador = hp_jogador - ataque_inimigo
+        hp_inimigo = hp_inimigo - ataque_jogador
+        if (hp_inimigo < 0):
+            hp_inimigo = 0
+        elif (hp_jogador < 0):
+            hp_jogador = 0
+        print("Hp jogador = {}".format(hp_jogador))
+        print(ataque_jogador)
+        print("Hp inimigo = {}".format(hp_inimigo))
+        print(ataque_inimigo)
+        continua = input("1- Continua")
